8ae14c2d35aa77fa40e5d6c10bdb4a3b9cf4dfb2
Hima Bindu Nandigama <binduhima246@gmail.com>
2018-02-18 21:51:17 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,8 +1,17 @@
+##  Getting dataset
 dataFile <- "./PowerConsumption/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+
+## Getting the subdata
 subData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+## Convert datetime
 datetime <- strptime(paste(subData$Date, subData$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 globalActivePower <- as.numeric(subData$Global_active_power)
-png("plot2.png", width=480, height=480)
+
+## Generate plot
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+
+## Save to plot2.png
+png("plot2.png", width=480, height=480)
 dev.off()
